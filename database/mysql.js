ade8c5cd86136ae3a8193ae81c74a6fe6a5a0c7d
AlfonsoConejo <hconejo@easysoft.com.mx>
2024-01-29 21:06:52 -0600

    Typo fixed

--- database/mysql.js
+++ database/mysql.js
@@ -141,7 +141,7 @@ function pasarVentaACancelacion(data){
 //Eliminamos de la tabla de ventas todos los datos de un folio específico
 function eliminarVenta(data){
 	return new Promise((resolve,reject) => {
-		connection.query( `DELETE FROM ventas WHERE folio ='${data[0].folio}'; `,
+		connection.query( `DELETE FROM ventas WHERE folioV ='${data[0].folio}'; `,
 	  	(err, result) => {
 	    	if (err) throw err;
 	  	});
